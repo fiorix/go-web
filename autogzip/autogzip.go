41c57dc1a70825a5cf5cc3b5a4a04e010e9ee557
fiorix <fiorix@gmail.com>
2013-04-04 11:07:29 -0400

    support for HandlerFunc

--- autogzip/autogzip.go
+++ autogzip/autogzip.go
@@ -26,12 +26,18 @@ func (w ResponseWriter) Write(b []byte) (int, error) {
 //
 // Usage:
 //
-//	func IndexHandler(w http.ResponseWriter, req *http.Request) {
-//		fmt.Fprintln(w, "Hello, world")
+//	func DL1Handler(w http.ResponseWriter, req *http.Request) {
+//		fmt.Fprintln(w, "foobar")
 //	}
 //
+//	func DL2Handler(w http.ResponseWriter, req *http.Request) {
+//		fmt.Fprintln(w, "zzz")
+//	}
+//
+//
 //	func main() {
-//		http.HandleFunc("/", IndexHandler)
+//		http.HandleFunc("/download1", DL1Handler)
+//		http.HandleFunc("/download2", DL2Handler)
 //		http.ListenAndServe(":8080", autogzip.Handle(http.DefaultServeMux))
 //	}
 func Handle(h http.Handler) http.HandlerFunc {
@@ -44,5 +50,35 @@ func Handle(h http.Handler) http.HandlerFunc {
 		gz := gzip.NewWriter(w)
 		defer gz.Close()
 		h.ServeHTTP(ResponseWriter{Writer: gz, ResponseWriter: w}, r)
+	}
+}
+
+// HandleFunc provides on-the-fly gzip encoding for other handler functions.
+//
+// Usage:
+//
+//	func IndexHandler(w http.ResponseWriter, req *http.Request) {
+//		fmt.Fprintln(w, "Hello, world")
+//	}
+//
+//	func DL1Handler(w http.ResponseWriter, req *http.Request) {
+//		fmt.Fprintln(w, "foobar")
+//	}
+//
+//	func main() {
+//		http.HandleFunc("/", IndexHandler)
+//		http.HandleFunc("/download1", autogzip.HandleFunc(DL1Handler))
+//		http.ListenAndServe(":8080", nil)
+//	}
+func HandleFunc(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			fn(w, r)
+			return
+		}
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
+		fn(ResponseWriter{Writer: gz, ResponseWriter: w}, r)
 	}
 }
