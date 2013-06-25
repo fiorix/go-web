70fe9d6a06279aae5c3ccae7380f915b0414a365
Alexandre Fiori <fiorix@gmail.com>
2013-06-24 23:25:18 -0400

    fix file cmdline for linux

--- templates/angular-signup-app/bootstrap.sh
+++ templates/angular-signup-app/bootstrap.sh
@@ -5,7 +5,7 @@ if [ "x$1" == "x" ]; then
 	exit 1
 fi
 for f in `find . -type f | grep -vE '(git/|lib/)'`; do
-	if [ "`file -I $f | grep -E '(text|xml)'`" != "" ]; then
+	if [ "`file --mime-type $f | grep -E '(text|xml)'`" != "" ]; then
 		sed -e s,angular-signup-app,$1,g < $f > $$.tmp
 		mv $$.tmp $f
 	fi
