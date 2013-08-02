13a23fbcc998504477d22f3e088e0bf060661a9a
Alexandre Fiori <fiorix@gmail.com>
2013-08-02 14:25:48 -0400

    Ignoring vendor/

--- templates/admin-panel/bootstrap.sh
+++ templates/admin-panel/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 name=`basename $PWD`
-for f in `find . -type f | grep -vE '(git/|lib/)'`; do
+for f in `find . -type f | grep -vE '(git/|vendor/)'`; do
 	if [ "`file --mime-type $f | grep -E '(text|xml)'`" != "" ]; then
 		sed -e s,%template%,${name},g < $f > $$.tmp
 		mv $$.tmp $f
